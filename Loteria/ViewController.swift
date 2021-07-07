a1f2fc1e08773a2d20c8247e13fda095f354770d
Wellington Alves Gomes <wellington.alves.gomes@gmail.com>
2021-07-07 11:10:30 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Wellington Alves on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
